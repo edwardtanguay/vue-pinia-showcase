4f1abcdc9f0969ff77019df7ede456d3f90c9ffd
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-14 01:20:29 +0100

    tilt

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -3,6 +3,6 @@
 <template>
 	<main>
 		<p>This is the welcome page.</p>
-		<img class="w-[20rem] mt-3 rounded" src="@/assets/images/pinia.jpg" />
+		<img class="w-[20rem] mt-3 rounded -rotate-6 ml-6" src="@/assets/images/pinia.jpg" />
 	</main>
 </template>
