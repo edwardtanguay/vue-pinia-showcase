0cb9d61e465006e18a06dc62501b0ee5869c8115
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-16 16:20:42 +0100

    skill view cleaned

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
 import { useAppStore } from "../stores/AppStore";
-import { ref } from "vue";
 
 const appStore = useAppStore();
-
-const count = ref(0);
 </script>
 <template>
-	<p>count = {{ count }}</p>
 	<div v-if="appStore.skills.length === 0">Loading...</div>
 	<div v-if="appStore.skills.length > 0">
 		<h2>There are currently {{ appStore.skills.length }} skills:</h2>
-		<div v-for="skill in appStore.skills" :key="skill.id" :class="skill.isProcessing && 'opacity-60'">
+		<div
+			v-for="skill in appStore.skills"
+			:key="skill.id"
+			:class="skill.isProcessing && 'opacity-70'"
+		>
 			<button
 				class="bg-slate-400 px-2 py-1 rounded text-xs mt-2"
 				:disabled="skill.isProcessing"
 				@click="
 					skill.isProcessing = true;
 					appStore.deleteSkill(skill);
-					count++
 				"
 			>
-				Delete
-				<span v-if="skill.isProcessing">...</span>
+				<span v-if="!skill.isProcessing">Delete</span>
+				<span v-if="skill.isProcessing" class="italic"
+					><v-icon name="fa-spinner" animation="spin" scale=".8" />
+					Deleting...</span
+				>
 			</button>
 			{{ skill.name }}
 		</div>
